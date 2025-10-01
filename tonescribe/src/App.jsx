9d0af068bc05644400fc61f344f1a0ae38acf970
Mariam Dobie <mariamdobie@gmail.com>
2025-10-01 17:45:57 -0400

    Fixed local setup

--- tonescribe/src/App.jsx
+++ tonescribe/src/App.jsx
@@ -1,9 +1,13 @@
 //import {useState} from "react" //A hook, a built-in function that allow your components to store user input
 import Navbar from "./components/Navbar";
+import textBox from "./components/textBox";
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <textBox/>
+    </>
   )
 }
 export default App;
---
+++ tonescribe/src/components/textBox.jsx
@@ -0,0 +1,11 @@
+import reactDOM from "react";
+
+function textBox(){
+    return(
+        <body>
+            <h1>textBox</h1>
+        </body>
+    )
+}
+
+export default textBox;
