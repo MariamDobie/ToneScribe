c7e962ff811426a457c4425652fa0fdac60bb949
Mariam Dobie <mariamdobie@gmail.com>
2025-10-08 23:50:36 -0400

    Created Navbar layout

--- tonescribe/src/components/Navbar.jsx
+++ tonescribe/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 function Navbar(){
     return(
-        <header>
+        <header className = "navbar">
+            <h1 className = "logo">ToneScribe</h1>
             <nav>
-                <h1 className="logo">ToneScribe</h1>
+                <ul className = "nav-links">
+                    <li><a herf="#">Home</a></li>
+                    <li><a herf="#">Analyze</a></li>
+                    <li><a herf="#">About</a></li>
+                </ul>
             </nav>
         </header>
     )
